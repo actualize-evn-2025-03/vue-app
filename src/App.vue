c5c498d6ea91b622973d6a9ac0b04f3c1c16558c
Leon Shimizu <lmshimizu@gmail.com>
2025-07-15 09:54:23 +1000

    Implement Employee new/create

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
 
+    <employee-form @add:employee="addEmployee" />
     <employee-table :employees="employees" />
   </div>
 </template>
 
 <script>
 import EmployeeTable from './components/EmployeeTable.vue';
+import EmployeeForm from './components/EmployeeForm.vue';
 
 export default {
   name: 'App',
   components: {
-    EmployeeTable
+    EmployeeTable,
+    EmployeeForm
   },
   data() {
     return {
@@ -38,6 +41,18 @@ export default {
           email: 'leon@test.com',
         }
       ]
+    }
+  },
+  methods: {
+    addEmployee(employee) {
+      const lastId =
+        this.employees.length > 0
+          ? this.employees[this.employees.length - 1].id
+          : 0;
+      const id = lastId + 1;
+      const newEmployee = { ...employee, id };
+
+      this.employees = [...this.employees, newEmployee];
     }
   }
 }
